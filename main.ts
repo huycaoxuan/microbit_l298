6fe9c1b162fa87d40e62d5dd5c8238c2f3cc23c2
Huy Cao <33714452+huycaoxuan@users.noreply.github.com>
2022-10-19 21:41:10 +0700

    Them phan dieu khien dong co servo

--- main.ts
+++ main.ts
@@ -8,21 +8,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 4) {
         Phai()
     } else if (receivedNumber == 5) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+        if (goc01 < 180) {
+            goc01 += 10
+            pins.servoWritePin(AnalogPin.P1, goc01)
+        }
     } else if (receivedNumber == 6) {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
+        if (goc01 >= 10) {
+            goc01 += -10
+            pins.servoWritePin(AnalogPin.P1, goc01)
+        }
     } else if (receivedNumber == 0) {
         Dung()
     }
@@ -57,3 +51,8 @@ function Trai () {
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
+let goc01 = 0
+goc01 = 90
+let goc02 = 90
+pins.servoWritePin(AnalogPin.P1, 90)
+pins.servoWritePin(AnalogPin.P2, 90)
